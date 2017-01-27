cbdd38b42a5358575e3e4bdedf05f2ca1808bad7
Ibrahim Khan <ibrahim@ibrahims-macbook.local>
2017-01-27 10:03:46 -0800

    Initial Commit

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Ibrahim Khan on 1/27/17.
+//  Copyright © 2017 IbrahimInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
