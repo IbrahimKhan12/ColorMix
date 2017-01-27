2246a91a7a56b34aafbcb04ab9e3814fd1711b5c
Ibrahim Khan <ibrahim@ibrahims-macbook.local>
2017-01-27 10:31:18 -0800

    colorView.backgroundColor = .black

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -13,13 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        colorView.backgroundColor = .black
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    @IBOutlet weak var colorView: UIView!
+    
 }
-
